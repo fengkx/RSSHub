57664fa7f4741227bf3b83b9241b76764dd328f9
fengkx <liangkx8237@gmail.com>
2025-01-16 00:35:01 +0800

    feat: vercel try

--- api/vercel.js
+++ api/vercel.js
@@ -1,28 +1,33 @@
-import 'tsx';
-import { createRequire } from 'module';
+// import 'tsx';
+// import { createRequire } from 'module';
 
-// export const config = {
-//     runtime: 'nodejs',
-// };
+const { createServer } = require("http");
 
-const require = createRequire(import.meta.url);
+// // export const config = {
+// //     runtime: 'nodejs',
+// // };
 
-const { setConfig } = require('../lib/config');
-setConfig({
-    NO_LOGFILES: true,
-});
+// const require = createRequire(import.meta.url);
+
+// const { setConfig } = require('../lib/config');
+// setConfig({
+//     NO_LOGFILES: true,
+// });
 
 // const { handle } = require('@hono/node-server/vercel');
-const { default: app } = await import('../lib/app');
-const { default: logger } = require('../lib/utils/logger');
+// const { default: app } = await import('../lib/app');
+// const { default: logger } = require('../lib/utils/logger');
 
-logger.info(`🎉 RSSHub is running! Cheers!`);
-logger.info('💖 Can you help keep this open source project alive? Please sponsor 👉 https://docs.rsshub.app/sponsor');
+// logger.info(`🎉 RSSHub is running! Cheers!`);
+// logger.info('💖 Can you help keep this open source project alive? Please sponsor 👉 https://docs.rsshub.app/sponsor');
 
 
 
 // export default handle(app);
 
-export default function a(req, res) {
-    res.end('Hello from vercel');
-}
+
+
+export default createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": 'text/plain'})
+    res.end('Hello RSSHub')
+})
